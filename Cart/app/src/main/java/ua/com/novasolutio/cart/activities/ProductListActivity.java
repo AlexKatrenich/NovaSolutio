2ce052923c3ea75cc86778cb5acd6c099748a69a
AlexKatrenich <alex.katrenich@gmail.com>
2019-03-04 15:27:48 +0200

    Update Android Studio/added entity class Product

--- Cart/app/src/main/java/ua/com/novasolutio/cart/activities/ProductListActivity.java
+++ Cart/app/src/main/java/ua/com/novasolutio/cart/activities/ProductListActivity.java
@@ -1,7 +1,17 @@
 package ua.com.novasolutio.cart.activities;
 
+
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import ua.com.novasolutio.cart.R;
 
 public class ProductListActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+
+    }
 }
---
+++ Cart/app/src/main/java/ua/com/novasolutio/cart/data/Product.java
@@ -0,0 +1,73 @@
+package ua.com.novasolutio.cart.data;
+
+import java.util.Objects;
+
+
+/* Клас, що описує дані про сутність продукт */
+
+public class Product {
+    private int mID;
+    private String mCaption;
+    private int mPrice;
+    private int mRate;
+
+    public Product() {
+    }
+
+    public Product(int mID, String mCaption, int mPrice, int mRate) {
+        this.mID = mID;
+        this.mCaption = mCaption;
+        this.mPrice = mPrice;
+        this.mRate = mRate;
+    }
+
+    public Product(int mID, String mCaption) {
+        this.mID = mID;
+        this.mCaption = mCaption;
+    }
+
+    public int getmID() {
+        return mID;
+    }
+
+    public void setmID(int mID) {
+        this.mID = mID;
+    }
+
+    public String getmCaption() {
+        return mCaption;
+    }
+
+    public void setmCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public int getmPrice() {
+        return mPrice;
+    }
+
+    public void setmPrice(int mPrice) {
+        this.mPrice = mPrice;
+    }
+
+    public int getmRate() {
+        return mRate;
+    }
+
+    public void setmRate(int mRate) {
+        this.mRate = mRate;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Product)
+            return this.getmID() == ((Product) obj).getmID();
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mID, mCaption);
+    }
+}
