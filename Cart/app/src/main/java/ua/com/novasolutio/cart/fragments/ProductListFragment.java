e7d069a5851ed4c2cb6c3d9f9f63dce9eb49b5c4
AlexKatrenich <alex.katrenich@gmail.com>
2019-03-05 10:58:49 +0200

    added bottom navigation menu on activity xml

---
+++ Cart/app/src/main/java/ua/com/novasolutio/cart/fragments/ProductListFragment.java
@@ -0,0 +1,8 @@
+package ua.com.novasolutio.cart.fragments;
+
+import android.support.v4.app.Fragment;
+
+/* Фрагмент для відображення списку товарів */
+public class ProductListFragment extends Fragment {
+
+}
