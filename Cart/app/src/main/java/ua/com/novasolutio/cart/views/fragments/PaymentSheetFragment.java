0cebd4e7e34806814c2e7f6b40503be94415db78
AlexKatrenich <alex.katrenich@gmail.com>
2019-04-08 12:27:58 +0300

    відредаговано відображення modal bottom sheet

--- Cart/app/src/main/java/ua/com/novasolutio/cart/views/fragments/PaymentSheetFragment.java
+++ Cart/app/src/main/java/ua/com/novasolutio/cart/views/fragments/PaymentSheetFragment.java
@@ -1,12 +1,17 @@
 package ua.com.novasolutio.cart.views.fragments;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import ua.com.novasolutio.cart.R;
 
@@ -14,6 +19,25 @@ import ua.com.novasolutio.cart.R;
 public class PaymentSheetFragment extends BottomSheetDialogFragment {
     public PaymentSheetFragment() {
         // потрібен пустий конструктор
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // задаємо розміри відкриття діалогу(щоб відкривався в повному розмірі)
+        BottomSheetDialog dialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                BottomSheetDialog d = (BottomSheetDialog) dialog;
+
+                FrameLayout bottomSheet = (FrameLayout) d.findViewById(android.support.design.R.id.design_bottom_sheet);
+                BottomSheetBehavior.from(bottomSheet).setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        return dialog;
     }
 
     @Override
